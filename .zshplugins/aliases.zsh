26aa5a6afdd83b209df60973f55b425922586c27
Stayfunn1 <stevensteenge0@gmail.com>
2023-04-17 15:25:14 +0200

    ZSH Plugins

---
+++ .zshplugins/aliases.zsh
@@ -0,0 +1,25 @@
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+
+alias ll='ls -lh'
+alias la='ls -lah'
+alias ls='ls --color=auto'
+
+alias grep='grep --color=auto'
+
+alias mkdir='mkdir -pv'
+
+alias mv='mv -i'
+alias cp='cp -i'
+alias rm='rm -i'
+alias ln='ln -i'
+
+alias rick='firefox https://www.youtube.com/watch\?v=dQw4w9WgXcQ'
+
+#System only alias
+alias np='python /home/steven/git/spotify-now-playing-terminal/main.py'
+
+
+
+
